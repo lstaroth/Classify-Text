2bea816106c0ea95c9e9d6cdf63d9a5cb181ed60
joliph <zhjf1996@163.com>
2018-02-13 00:23:13 +0800

    readdata

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-测试
+**学习研究项目：基于微博评论的数据挖掘与情感分析**  
+
+
+**项目简介**  
+学习cnn，rnn在实际环境下的应用，提升实践能力，了解深度学习在自然语言处理方面的进展。  
+分别测试cnn与rnn模型在微博评论分类任务下的表现  
+
+cnn_for_emotion_classify  
+模型架构参考论文：[Implementing a CNN for Text Classification](http://www.wildml.com/2015/12/implementing-a-cnn-for-text-classification-in-tensorflow/)  
+
+lstm_for_emotion_classify  
+待完善
+
+word2vec:  
+项目使用的词向量：embedding_64.bin(1.5G)  
+训练语料：百度百科800w+条 20G+搜狐新闻400w+条 12G+小说：90G左右  
+模型参数：window=5 min_count=5 size=64  
+下载链接：[百度网盘链接](https://pan.baidu.com/s/1o7MWrnc) 密码:wzqv  
+原博主主页：[新浪微博](https://weibo.com/u/6131343391?is_all=1)  
+
+
+**文件功能介绍**
+
+pos.txt：正面评价数据集  
+neg.txt：负面评价数据集  
+weibo.py：微博评论爬虫  
+readdata.py：为情感分析模型提供多种数据加载相关API
+
+
+
+
+**推荐运行环境**  
+
+python 3.6  
+tensorflow-gpu 1.4  
+gensim 3.3  
+Ubuntu 64bit / windows10 64bit  
---
+++ readdata.py
@@ -0,0 +1,70 @@
+# encoding: UTF-8
+import numpy as np
+import re
+import os
+import pickle
+import jieba
+
+
+def clean_str(string):
+    '''
+    接收string，返回去除各种符号的string
+    '''
+    string=re.sub("[^\u4e00-\u9fff]"," ",string)
+    string = re.sub(r"\s{2,}", " ", string)
+    return string
+
+
+def split_str(string):
+    '''
+    接收string，返回各个词间用空格隔开的string
+    '''
+    return " ".join([word for word in jieba.cut(string,HMM=True)])
+
+
+def get_cleaned_list(file_path):
+    '''
+    接收文件全路径，返回次txt文件的分词好的列表
+    '''
+    f=open(file_path,'r',encoding="utf8")
+    lines=list(f.readlines())
+    lines=[clean_str(split_str(line)) for line in lines]
+    return lines
+
+
+def padding_sentences(no_padding_lists, padding_token='<PADDING>',padding_sentence_length = None):
+    '''
+    接收句子列表，将所有句子填充为一样长
+    '''
+    all_sample_lists=[sentence.split(' ') for sentence in no_padding_lists]
+    if padding_sentence_length != None:
+        max_sentence_length=padding_sentence_length
+    else:
+        max_sentence_length=max([len(sentence) for sentence in all_sample_lists])
+    for sentence in all_sample_lists:
+        if len(sentence) > max_sentence_length:
+            sentence=sentence[:max_sentence_length]
+        else:
+            sentence.extend([padding_token] * (max_sentence_length - len(sentence)))
+    return (all_sample_lists,max_sentence_length)
+
+
+def get_all_data_from_file(positive_file_path,negative_file_path):
+    '''
+    positive_file_path:正评价txt全路径
+    negative_file_path:负评价txt全路径
+    '''
+    positive_sample_lists=get_cleaned_list(positive_file_path)
+    negative_sample_lists=get_cleaned_list(negative_file_path)
+    positive_label_lists=[[0,1] for _ in positive_sample_lists]
+    negative_label_lists=[[1,0] for _ in negative_sample_lists]
+
+
+    all_sample_lists=positive_sample_lists + negative_sample_lists  #样本为list类型！！
+    all_sample_lists, max_sentences_length = padding_sentences(all_sample_lists)  #样本为list类型！！
+    all_label_arrays=np.concatenate([positive_label_lists, negative_label_lists], 0)  #标签为array类型
+
+    return (all_sample_lists,all_label_arrays,max_sentences_length)
+
+
+#all_sample_lists,all_label_arrays,max_sentences_length=get_all_data_from_file("D:\\Emotion-on-Comments\\pos.txt","D:\\Emotion-on-Comments\\neg.txt")
