4ef07e4ed99f57fff8d5befc8e92d34bd996b114
jiexixijie <han942533279@gmail.com>
2018-05-22 23:20:19 +0800

    修复

--- weibo.py
+++ weibo.py
@@ -34,9 +34,10 @@ class visitor():
                 "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:41.0) Gecko/20100101 Firefox/41.0"
             }
         )
+
         data = {
             "cb": "gen_callback",
-            "fp": '{"os":"1","browser":"Gecko58,0,0,0","fonts":"undefined","screenInfo":"1536*864*24","plugins":""}'
+            "fp": '{"os":"1","browser":"Gecko60,0,0,0","fonts":"undefined","screenInfo":"1536*864*24","plugins":""}'
         }
         response = S.post(url, data=data)
         pattren=re.compile(r"\((.*)\)")
@@ -49,15 +50,32 @@ class visitor():
             where=3
         else:
             where=2
-        # print(tid,confidence,where)
-        url="https://passport.weibo.com/visitor/visitor?a=incarnate&t="\
-            +str(tid)+"&w="\
-            +str(where)+"&c="\
-            +str(confidence)+"&cb=cross_domain&from=weibo"
+        while(len(str(confidence))<3):
+            confidence="0"+str(confidence)
+        #tid="KCEsfUfkAmyXExt9tiPN61f32Vvh4wViWQaeHptBZLc="
+        #手动编码格式转换
+        tid=tid.replace("+","%2b")
+        tid=tid.replace("=","%3d")
+
+        url="https://passport.weibo.com/visitor/visitor?a=incarnate"\
+            "&t="+str(tid)+ \
+            "&w=" + str(where) + \
+            "&c="+str(confidence)+\
+            "&gc="\
+            "&cb=cross_domain" \
+            "&from=weibo"
         response=S.get(url)
         data=pattren.findall(response.text)[0]
-        dict_data=json.loads(data)["data"]
-        sub=dict_data["sub"]
+        #https://passport.weibo.com/visitor/visitor?a=incarnate&t=hVEmh0nd32++OFXP3wiB6b05C9A5L38fmq7ArFKTNq8=&w=2&c=095&gc=&cb=cross_domain&from=weibo'
+        #https://passport.weibo.com/visitor/visitor?a=incarnate&t=+A1gVsii+zY9OI9v/e+o1lfhlTPQ20U3Fkuz8nn/7rU=&w=2&c=095&gc=&cb=cross_domain&from=weibo&_rand=0.42337865580692513
+        dict_data = json.loads(data)
+        if "succ" not in dict_data["msg"]:
+            printf("tid不合法")
+            printf("dict_data:",dict_data)
+            self.fail=True
+            return None,None
+        dict_data=dict_data["data"]
+        sub=dict_data["sub"]    #没有
         subp=dict_data["subp"]
         # print(sub,subp)
         url="https://login.sina.com.cn/visitor/visitor?a=crossdomain&cb=return_back"\
@@ -94,7 +112,9 @@ class visitor():
         content=str(content).replace("\n"," ")
         print("爬取ing\n\t",content)
         # #获取异步加载url中的id
-        pattern=re.compile(r"\d\/(.*)\?type")
+        pattern=re.compile(r"\d\/(.*)\?.*type")
+        # https://weibo.com/5678693647/GaERmaQ33?filter=hot&root_comment_id=0&type=comment
+        # https://weibo.com/5678693647/GaERmaQ33?type=comment
         content=pattern.search(url).group(1)
         string1=str(self.base62(content[0]))
         string2=str(self.base62(content[1:5]))
@@ -103,11 +123,10 @@ class visitor():
         string3=str(self.base62(content[5:]))
         while (len(string3) < 7):
             string3 = "0" + string3
-        print(string1,string2,string3)
         id=string1+string2+string3
         return id
 
-    def catch_comments(self,page=1,past_html=None):
+    def catch_comments(self,page=1,past=None):
         #模拟异步加载
         #https://weibo.com/aj/v6/comment/big?ajwvr=6&id=4213083327566698&filter=hot&page=1
         path="./data"
@@ -134,11 +153,11 @@ class visitor():
         response=S.get(url)
         html=json.loads(response.text)["data"]["html"]
         #如果两次相同表示结束了 -。-
-        if past_html==html:
-            print("爬取结束")
-            # print(self.id)
-            print("共",page,"页")
-            return
+        # if past_html==html:
+        #     print("爬取结束")
+        #     # print(self.id)
+        #     print("共",page,"页")
+        #     return
         #搜索评论
         text=etree.HTML(html)
         #print(html)
@@ -161,6 +180,13 @@ class visitor():
             comment = comments[i].xpath("text()")
             comment = ",".join(comment[1:])[1:].strip()
             point = points[i]
+            if i==0:
+                now = {"len": len(comments), "comment":comment}
+                if now==past:
+                    print("爬取结束")
+                    return
+                else:
+                    past=now
             if point == "赞":
                 point = "0"
             #点赞数为权重0.2
@@ -177,7 +203,7 @@ class visitor():
     #     print("写入文件失败")
 
         page+=1
-        self.catch_comments(page,html)
+        self.catch_comments(page,past)
 
     #获取图片
     def catch_pictures(self,page=1,past_html=None):
@@ -241,6 +267,12 @@ if __name__=="__main__":
     if spider.fail==False:
         spider.catch_comments()
     # spider.catch_pictures()
+#https://weibo.com/1840483562/G48Ajgfhq?type=comment
+#https://weibo.com/aj/v6/comment/big?ajwvr=6&id=4209863153871988&filter=hot&page=1
+
+#https://weibo.com/2387903701/G5bn7s5CS?type=comment
+#https://weibo.com/aj/v6/comment/big?ajwvr=6&id=4212353576694814&filter=hot&page=12
+
 
 #------------------
 #      ~。~   nice
